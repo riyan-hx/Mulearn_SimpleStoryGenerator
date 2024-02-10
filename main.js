7087e06007a3e458414b339ca8ff88fa40adf0a6
riyan <riyansk255@gmail.com>
2024-02-10 16:42:54 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+
+const insertx = ["Willy the Goblin","Big Daddy","Father Christmas"];
+
+const inserty = ["the soup kitchen","Disneyland","the White House"];
+
+const insertz = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
+
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    let newStory = storyText;
+  let xItem = randomValueFromArray(insertx);
+  let yItem = randomValueFromArray(inserty);
+  let zItem = randomValueFromArray(insertz);
+
+  newStory=newStory.replaceAll(':insertx:',xItem)
+
+  newStory=newStory.replaceAll(':inserty:',yItem)
+    
+  newStory=newStory.replaceAll(':insertz:',zItem)
+
+    if(customName.value !== '') {
+      const name = customName.value;
+    newStory=newStory.replaceAll('Bob',name);
+
+    }
+
+    if(document.getElementById("uk").checked) {
+      weight = `${Math.round(300/14)} stone`;
+
+     temperature = `${Math.round((94-32) * 5 / 9)}centigrade`;
+    newStory=newStory.replaceAll('94 fahrenheit', temperature);
+    newStory=newStory.replaceAll('300', weight);
+
+
+
+    }
+
+    story.textContent = newStory ;
+    story.style.visibility = 'visible';
+  }
